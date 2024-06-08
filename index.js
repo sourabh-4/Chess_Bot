2e2390500b694bafaa877238207fa69e9c226068
Parth Rasal <parth.rasal@yahoo.com>
2024-06-08 12:37:03 +0530

    Created index.js
    Created index.js to segregate the script code
    from index.html

---
+++ index.js
@@ -0,0 +1,6 @@
+const element = document.createElement("div");
+    element.style.backgroundColor = "#769656";
+    element.style.width = "200px";
+    element.style.height = "200px";
+
+    document.getElementsByTagName("div")[0].appendChild(element);
