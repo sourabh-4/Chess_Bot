b5f2eef5f7a44245b9b76cf7356dfd836e0e6187
Parth Rasal <parth.rasal@yahoo.com>
2024-06-08 13:02:17 +0530

    Create functions to create chess board
    - Create function square to represent data of
    each square on board
    - Create function squareRow to create a 1D array
    of each row
    - Create function initGame to create a multidim.
    array of all squares
    
    Note: Modifications are to be made for squareRow
    function

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const element = document.createElement("div");
-    element.style.backgroundColor = "#769656";
-    element.style.width = "200px";
-    element.style.height = "200px";
+element.style.backgroundColor = "#769656";
+element.style.width = "200px";
+element.style.height = "200px";
 
-    document.getElementsByTagName("div")[0].appendChild(element);
+document.getElementsByTagName("div")[0].appendChild(element);
+
+// For each square
+function square(color, id, piece){
+    return {color, id, piece}
+};
+
+function squareRow(rowID){
+    const squareRow = [];
+    if (rowID % 2 == 0){
+        squareRow.push(square('white', 'something', null))
+    }
+    else{
+        squareRow.push(square('#769656', 'something', null))
+    }
+
+    return squareRow;
+};
+
+function initGame(){
+    const square = [];
+
+    for (let i = 8; i>0; i--){
+        square.push(squareRow(i));
+    }
+
+    return square;
+};
+
+console.log(initGame());
+
+export {initGame};
